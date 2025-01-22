a08697e6dd1993d5c6a15f5eab012ba6e00ebba5
Sware <shangfire@gmail.com>
2025-01-22 18:31:48 +0800

    初始化工程

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+ * @Author: shanghanjin
+ * @Date: 2025-01-22 17:32:42
+ * @LastEditTime: 2025-01-22 17:36:14
+ * @FilePath: \ControllerEmulator\src\main.rs
+ * @Description: 
+ */
+use mlua::lua;
+
+fn main() {
+    println!("Hello, world!");
+}
